a4907714b731ab2f7b527b6e2802ddd0cdedc443
Simen Gangstad <simen.gangstad@me.com>
2021-04-26 11:44:44 +0200

    Remove double quotes

--- 2 - Handwritten numbers/Task1.m
+++ 2 - Handwritten numbers/Task1.m
@@ -62,15 +62,15 @@ end
 for class_index = 1:size(confusion_matrix, 1)
     total_test_samples = sum(confusion_matrix(class_index, :));
     error_rate = 100 * (1 - confusion_matrix(class_index, class_index) / total_test_samples);
-    fprintf("Error rate for number %d is %.2f. Total test samples: %d\n", (class_index - 1), error_rate, total_test_samples);
+    fprintf('Error rate for number %d is %.2f. Total test samples: %d\n', (class_index - 1), error_rate, total_test_samples);
 end
 
 % Compute total error rate
 sum_without_diagonal = (sum(confusion_matrix, 'all') - sum(diag(confusion_matrix)));
 error_rate = 100 * sum_without_diagonal / sum(confusion_matrix, 'all');
-fprintf("Total error rate: %.2f\n", error_rate);
+fprintf('Total error rate: %.2f\n', error_rate);
 
-disp("Confusion matrix");
+disp('Confusion matrix');
 disp(confusion_matrix);
 
 
