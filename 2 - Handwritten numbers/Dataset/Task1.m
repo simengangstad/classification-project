cfce5ebb42e895a70bd36ea3ab83d64085a8fada
Geir Ola <gotvinne@stud.ntnu.no>
2021-04-26 11:43:47 +0200

    Clustering implemented, bad results

--- 2 - Handwritten numbers/Dataset/Task1.m
+++ 2 - Handwritten numbers/Dataset/Task1.m
@@ -9,7 +9,6 @@ test_data = data.testv;
 
 
 %% Iterative construction of distance matrix in chunks and confusion matrix
-
 confusion_matrix = zeros(10, 10);
 
 chunk_training_size = 6000;
@@ -70,16 +69,16 @@ end
 % Compute class error rate
 for class_index = 1:size(confusion_matrix, 1)
     total_test_samples = sum(confusion_matrix(class_index, :));
-    error_rate = confusion_matrix(class_index, class_index) / total_test_samples;
-    fprintf("Error rate for number %d is %d. Total test samples: %d\n", (class_index - 1), error_rate, total_test_samples);
+    error_rate = 1 - confusion_matrix(class_index, class_index) / total_test_samples;
+    fprintf('Error rate for number %d is %.2f. Total test samples: %d\n', (class_index - 1), error_rate * 100, total_test_samples);
 end
 
 % Compute total error rate
 sum_without_diagonal = (sum(confusion_matrix, 'all') - sum(diag(confusion_matrix)));
 error_rate = sum_without_diagonal / sum(confusion_matrix, 'all');
-fprintf("Total error rate: %d\n", error_rate);
+fprintf('Total error rate: %d\n', error_rate);
 
-disp("Confusion matrix");
+disp('Confusion matrix');
 disp(confusion_matrix);
 
 
